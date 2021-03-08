617ae92a40c421f7d4998afb130bf832b08e0006
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-03-08 20:45:42 -0300

    Create teste and colecoes files

--- Aula 3 - Colecoes/src/colecoes.java
+++ Aula 3 - Colecoes/src/colecoes.java
@@ -1,2 +1,13 @@
 public class colecoes {
+
+    /*
+    Coleção estática (com vetor) de inteiros
+    Cada elemento novo será colocado no próximo espaço disponivel no vetor
+    Buscar na coleção por um inteiro
+    Deletar da coleção um inteiro
+    Imprimir a coleção
+     */
+
+
+
 }
