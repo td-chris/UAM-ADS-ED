3f45a90a97b70ece6925eea3b39ea3f85a71e878
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-03-08 21:06:13 -0300

    Create teste

--- Aula 3 - Colecoes/src/teste.java
+++ Aula 3 - Colecoes/src/teste.java
@@ -2,5 +2,15 @@ public class teste {
 
     public static void main(String[] args) {
 
+        colecoes C = new colecoes(15);
+
+        C.inserir(15);
+        C.inserir(19);
+        C.inserir(19);
+        C.inserir(13);
+        C.inserir(19);
+        C.inserir(0);
+
+        C.imprimir();
     }
 }
