5df68fcbb52b1e86d20023998648ea15b5684988
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-03-15 19:36:03 -0300

    create pilhas construcutor

--- Aula 4 - Pilhas/src/Pilhas.java
+++ Aula 4 - Pilhas/src/Pilhas.java
@@ -1,5 +1,19 @@
 public class Pilhas {
     //Pilhas são conhecidas como stack
 
+    // Stack estática - usando vetores
+
+    // Stack estática de int
+
+    private int dade[];
+    private int topo;
+
+    //Construtor
+
+    public Pilhas(int max) {
+        this.dados = new int[max];
+        this.topo = 0;
+    }
+
 
 }
--- Aula 4 - Pilhas/src/teste.java
+++ Aula 4 - Pilhas/src/teste.java
@@ -1,3 +1,5 @@
 public class teste {
+    public static void main(String[] args) {
 
+    }
 }
