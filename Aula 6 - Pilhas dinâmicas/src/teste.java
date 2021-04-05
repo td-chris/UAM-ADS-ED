ff68dd2d48c8659f5d03277b632909ef66310c98
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-04-05 20:38:20 -0300

    Fix: Update teste class

--- Aula 6 - Pilhas dinâmicas/src/teste.java
+++ Aula 6 - Pilhas dinâmicas/src/teste.java
@@ -1,5 +1,16 @@
 public class teste {
     public static void main(String[] args) {
+        Stack s = new Stack();
 
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        s.push(6);
+
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
     }
 }
