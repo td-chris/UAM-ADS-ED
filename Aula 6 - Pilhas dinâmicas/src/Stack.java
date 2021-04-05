b6e92fed386355f11c33f8914f260220f499085b
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-04-05 20:18:32 -0300

    Feat: Create constructor and isEmpty in Stack class

--- Aula 6 - Pilhas dinâmicas/src/Stack.java
+++ Aula 6 - Pilhas dinâmicas/src/Stack.java
@@ -1,5 +1,18 @@
 public class Stack {
     // Pilha de Nos inteiros
 
+    private No topo;
 
+    // Construtor
+    public Stack() {
+        this.topo = null;
+    }
+
+    // Está vazio?
+    public boolean isEmpty() {
+        if (this.topo == null)
+            return true;
+        else
+            return false
+    }
 }
