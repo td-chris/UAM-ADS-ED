7fd61042dbe443f6a76bb7d350c4745ba52cba92
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-04-05 21:33:13 -0300

    Fix: Update teste class for the second exercise

--- Aula 6 - Pilhas dinâmicas/src/Stack.java
+++ Aula 6 - Pilhas dinâmicas/src/Stack.java
@@ -38,4 +38,6 @@ public class Stack {
 
         return aux.dado;
     }
+
+
 }
--- Aula 6 - Pilhas dinâmicas/src/teste.java
+++ Aula 6 - Pilhas dinâmicas/src/teste.java
@@ -1,16 +1,67 @@
+import java.util.Scanner;
+
 public class teste {
     public static void main(String[] args) {
+        // exercicio 1
+//        Stack s = new Stack();
+//
+//        s.push(3.8);
+//        s.push(4.5);
+//        s.push(5);
+//        s.push(6.99);
+//
+//        System.out.println(s.pop());
+//        System.out.println(s.pop());
+//        System.out.println(s.pop());
+//        System.out.println(s.pop());
+
+        // exercicio 2 - Notação pós fixa (+ - * /)
+
         Stack s = new Stack();
 
-        s.push(3);
-        s.push(4);
-        s.push(5);
-        s.push(6);
+        Scanner entrada = new Scanner(System.in);
+
+        String frase = entrada.nextLine();
+        entrada.close();
+
+        String fraseSeparada[] = frase.split(" ");
+
+//        for (int i = 0; i < fraseSeparada.length; i++) {
+//            String dado = fraseSeparada[i];
+//        }
+
+        for(String dado : fraseSeparada) {
+            if (dado.equals("+")){
+                double operador2 = s.pop();
+                double operador1 = s.pop();
+
+                double  resultado = operador1 + operador2;
+                s.push(resultado);
+            }else if (dado.equals("-")){
+                double operador2 = s.pop();
+                double operador1 = s.pop();
+
+                double resultado = operador1 - operador2;
+                s.push(resultado);
+            }else if (dado.equals("*")){
+                double operador2 = s.pop();
+                double operador1 = s.pop();
+
+                double resultado = operador1 * operador2;
+                s.push(resultado);
+            }else if (dado.equals("/")){
+                double operador2 = s.pop();
+                double operador1 = s.pop();
+
+                double resultado = operador1 / operador2;
+                s.push(resultado);
+            } else {
+                double valor = Double.parseDouble(dado);
+                s.push(valor);
+            }
+        }
 
         System.out.println(s.pop());
-        System.out.println(s.pop());
-        System.out.println(s.pop());
-        System.out.println(s.pop());
-        System.out.println(s.pop());
+
     }
 }
