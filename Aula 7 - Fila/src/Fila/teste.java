2ad6292fe60914dcd4c3026a138450d068b64131
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-04-12 20:46:07 -0300

    Feat: create functions on teste file

--- Aula 7 - Fila/src/Fila/teste.java
+++ Aula 7 - Fila/src/Fila/teste.java
@@ -1,6 +1,22 @@
 package Fila;
 
 public class teste {
+    public static void main(String[] args) {
+        Fila f = new Fila(5);
 
+        f.entra(1);
+        f.entra(2);
+        f.entra(3);
+        f.entra(4);
+        f.entra(5);
+
+        f.entra(6); //erro
+
+        while (!f.vazia()) {
+            System.out.println(f.sai());
+        }
+
+        System.out.println(f.sai());
+    }
 
 }
