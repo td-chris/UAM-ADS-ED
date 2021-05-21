23e5dc3f87005a8d0e9ee1c6c698346b1479910f
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-05-21 20:00:19 -0300

    Feat: create Lista class and its constructor

--- Aula 8 - Lista de exercícios/src/ListaDeExercicios/Lista.java
+++ Aula 8 - Lista de exercícios/src/ListaDeExercicios/Lista.java
@@ -1,4 +1,15 @@
 package ListaDeExercicios;
 
 public class Lista {
+    private No beggin;
+    private No end;
+    private int qtd;
+
+    //Constructor
+    public Lista() {
+        this.beggin = null;
+        this.end = null;
+        this.qtd = 0;
+    }
+
 }
