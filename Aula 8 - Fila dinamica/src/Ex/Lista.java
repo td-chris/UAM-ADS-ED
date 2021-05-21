07ce25c81a0623891ef5566376123437f616665f
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-05-20 21:49:03 -0300

    Feat: create the remove function on lista file

--- Aula 8 - Fila dinamica/src/Ex/Lista.java
+++ Aula 8 - Fila dinamica/src/Ex/Lista.java
@@ -98,4 +98,43 @@ public class Lista {
         this.add(elem, this.size());
     }
 
+    //Remover
+    public double remove(int pos) {
+        if (this.isEmpty() || pos >= this.size() || pos < 0){
+            System.out.println("Erro");
+            return -1.0;
+        }
+
+        if (pos == 0 && this.qtd == 1) {
+            No aux = this.inicio;
+            this.inicio = null;
+            this.fim = null;
+
+            this.qtd--;
+            return aux.dado;
+        }
+
+        if (pos == 0) {
+            No aux = this.inicio;
+            this.inicio = this.inicio.prox;
+
+            this.qtd--;
+            return aux.dado;
+        }
+
+        No anterior = this.inicio;
+        int count = 0;
+
+        while (count != pos-1) {
+            anterior = anterior.prox;
+            count++;
+        }
+
+        No removido = anterior.prox;
+        anterior.prox = removido.prox;
+
+        this.qtd--;
+        return removido.dado;
+    }
+
 }
