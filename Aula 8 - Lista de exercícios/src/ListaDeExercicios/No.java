6ed27fd18692fec5469f17d7230d0abc82e94a8c
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-05-21 19:54:07 -0300

    Feat: create No class and it's constructor

--- Aula 8 - Lista de exercícios/src/ListaDeExercicios/No.java
+++ Aula 8 - Lista de exercícios/src/ListaDeExercicios/No.java
@@ -1,4 +1,13 @@
 package ListaDeExercicios;
 
 public class No {
+    public int data;
+    public No prox;
+
+    // constructor
+    public No(int data) {
+        this.data = data;
+        this.prox = null
+    }
+
 }
