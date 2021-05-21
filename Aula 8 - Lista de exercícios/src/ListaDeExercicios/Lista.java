dd4dee2b2a890ead82c0dc3fa8471204df98537d
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-05-21 20:18:14 -0300

    Feat: create the add method on Lista class

--- Aula 8 - Lista de exercícios/src/ListaDeExercicios/Lista.java
+++ Aula 8 - Lista de exercícios/src/ListaDeExercicios/Lista.java
@@ -43,4 +43,49 @@ public class Lista {
         return false;
     }
 
+    //Add
+    public void add(int elem, int pos) {
+        if (pos > this.size() || pos < 0)
+            return;
+
+        if (pos == 0 && this.isEmpty()) {
+            No newNo = new No(elem);
+
+            this.beggin = newNo;
+            this.end = newNo;
+            this.qtd++;
+        }
+
+        if (pos == 0 && !this.isEmpty()){
+            No newNo = new No(elem);
+
+            newNo.next = this.beggin;
+            this.beggin = newNo;
+            this.qtd++;
+        }
+
+        if (pos == this.size()) {
+            No newNo = new No(elem);
+
+            this.end.next = this.end;
+            this.end = newNo;
+            this.qtd++;
+        }
+
+        No newNo = new No(elem);
+
+        No aux = this.beggin;
+        int count = 0;
+
+        while (count != pos-1){
+            aux = aux.next;
+            count++;
+        }
+
+        newNo.next = aux.next;
+        aux.next = newNo;
+
+        this.qtd++;
+    }
+
 }
