530d2a90036246dd7de47339e9ce35efa136d8c0
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-06-03 19:51:49 -0300

    Feat: create the constructor of ListaDupla file

--- Aula 9 - Lista encadeada/src/aula9/ListaDupla.java
+++ Aula 9 - Lista encadeada/src/aula9/ListaDupla.java
@@ -1,4 +1,17 @@
 package aula9;
 
 public class ListaDupla {
+
+    private No inicio;
+    private No fim;
+    private int qtd;
+
+    // Construtor
+
+    public ListaDupla() {
+        this.inicio = null;
+        this.fim = null;
+        this.qtd = 0;
+    }
+
 }
